fbc711a427eb3d8daddb42418e56d8f0fcbd042b
mmwihaki <marymwihaki16@gmail.com>
2023-04-26 00:06:05 +0300

    a function that adds two integers and returns the result.

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
+  * add - entry point
   *
+  * @x: integer to scan
+  * @y: integer to scan
   *
+  * Return: Always 0 (success)
   */
 int add(int x, int y)
 {
