2d61c57ea6b6a29a57a7953bbfe3a2c8375d5e54
mmwihaki <marymwihaki16@gmail.com>
2023-04-26 00:28:02 +0300

    a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * print_to_98 - entry point
+  *
+  * @n: integer to scan
+  *
+  * description - print all natural numbers
+  */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d", n++);
+	}
+}
