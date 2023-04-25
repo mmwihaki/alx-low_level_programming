b35c01d44c34303e2449f4dc914c477adac03fcd
mmwihaki <marymwihaki16@gmail.com>
2023-04-26 00:41:13 +0300

    a function that adds two integers and returns the result.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,8 +10,15 @@
   */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n <= 98 && n >= 98)
 	{
-		printf("%d\n", n++);
+		if (n <= 98)
+		{
+			printf("%d\n", n++);
+		}
+		else
+		{
+			printf("%d\n", n--);
+		}
 	}
 }
