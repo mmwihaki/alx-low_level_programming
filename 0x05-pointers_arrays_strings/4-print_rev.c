9ddfe8a7c8093545e17c4a10460e419053fc3511
mmwihaki <marymwihaki16@gmail.com>
2023-05-02 20:18:54 +0300

    function that prints a string, followed by a new line, to stdout.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
   * print_rev - print string in reverse
   *
@@ -18,7 +17,7 @@ void print_rev(char *s)
 		c++;
 	}
 
-	for (c = 1; c >= 0; c--)
+	for (c -= 1; c >= 0; c--)
 	{
 		_putchar(s[c]);
 	}
