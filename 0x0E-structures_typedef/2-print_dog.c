787cf1946e6e06bf57b620e8c45b738694f58df8
mmwihaki <marymwihaki16@gmail.com>
2023-05-22 18:41:41 -0700

    betty coding style

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,9 +3,9 @@
 /**
  * print_dog - print dog
  *
- * @d - Pointer to the struct dog to be printed
+ * @d: Pointer to the struct dog to be printed
  *
- * Description - print dog
+ * description - print dog
  *
  * If d is NULL, it prints nothing
  */
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
 	printf("Age: %.1f\n", d->age);
-	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
