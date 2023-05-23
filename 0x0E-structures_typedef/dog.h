52a20c1207bba45ad6aed4428ecc3f5269ac35ab
mmwihaki <marymwihaki16@gmail.com>
2023-05-22 17:57:37 -0700

    betty coding style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog{
+/**
+ * struct dog - Poppy
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: new struct dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
