cc3f861051397c9be6fd207ac18507443e4cb7ba
mmwihaki <marymwihaki16@gmail.com>
2023-06-03 01:33:35 +0300

    betty lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,19 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stddef.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
-	char* str;
+	char *str;
 	unsigned int len;
-	struct list_s* next;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
