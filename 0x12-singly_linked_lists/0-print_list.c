3a9ffb1330c0d3904ab9b124b798d5f7f3c2d593
mmwihaki <marymwihaki16@gmail.com>
2023-06-03 01:31:29 +0300

    add string length

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
 /**
   * print_list - a function that prints all the elements of a list
   *
@@ -14,23 +16,18 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 	const list_t *j = h;
+	size_t len;
 
 	if (h == NULL)
 	{
-		printf("[0] (nil)");
+		printf("[0] (nil)\n");
 		return (i);
 	}
 
 	for (j = h; j != NULL; j = j->next)
 	{
-		if(j->str == NULL)
-		{
-			printf("[0] (nil)");
-		}
-		else
-		{
-			printf("%s\n", j->str);
-		}
+		len = j->str ? strlen(j->str) : 0;
+		printf("[%lu] %s\n", len, j->str);
 		i++;
 	}
 	return (i);
