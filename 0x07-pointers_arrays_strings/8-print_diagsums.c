df8ea93fbe28a632e620fa6f9a143c4a70eebf46
mmwihaki <marymwihaki16@gmail.com>
2023-06-05 06:54:39 -0700

    betty coding style

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		diagsum1 += a[i * size +i];
+		diagsum1 += a[i * size + i];
 		diagsum2 += a[i * size + size - i - 1];
 	}
 
