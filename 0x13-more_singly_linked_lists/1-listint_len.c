fa3be769073ca2c842ec7c63e07feec127198c21
mmwihaki <marymwihaki16@gmail.com>
2023-06-06 07:58:01 -0700

    betty coding style

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,7 +3,7 @@
 /**
  * listint_len - check number of elements in a linkedin list
  *
- * @h - head node
+ * @h: head node
  *
  * description - return number of elements
  *
@@ -14,7 +14,7 @@ size_t listint_len(const listint_t *h)
 	size_t count = 0;
 	const listint_t *j;
 
-	for (j =h; j!= NULL; j = j->next)
+	for (j = h; j != NULL; j = j->next)
 	{
 		count++;
 	}
