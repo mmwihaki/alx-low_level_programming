ff5aa373c8e8ca74c035bcdbedb1a0b265d99e16
mmwihaki <marymwihaki16@gmail.com>
2023-06-13 13:42:35 -0700

    handle malloc return

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	i = malloc(sizeof(int *) *height);
+	i = malloc(sizeof(int *) * height);
 
 	for (x = 0; x < height; x++)
 	{
