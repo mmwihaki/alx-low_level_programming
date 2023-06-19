9a0c239043d1ced091b9ff3064183ebc69d4fdba
mmwihaki <marymwihaki16@gmail.com>
2023-06-19 16:26:39 -0700

    a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Poppy
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * description - new struct dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
