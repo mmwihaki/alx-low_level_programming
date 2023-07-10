15ff11c1ebcbbf699f5ed1457ac27a31f915a2b9
mmwihaki <marymwihaki16@gmail.com>
2023-07-10 12:48:13 -0700

    betty coding style

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,31 +5,36 @@
 #include <unistd.h>
 #include <string.h>
 /**
-*/
+ * create_file - create file
+ * @filename: filename
+ * @text_content: content
+ * description - create file
+ * Return: 1
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int filedoc;
 	ssize_t b_write;
-	
+
 	if (filename == NULL)
 		return (-1);
-	
+
 	filedoc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	
+
 	if (filedoc == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
 		b_write = write(filedoc, text_content, strlen(text_content));
-		
+
 		if (b_write == -1)
 		{
 			close(filedoc);
 			return (-1);
 		}
 	}
-	
+
 	close(filedoc);
 	return (1);
 }
