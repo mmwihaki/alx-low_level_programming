4107d371b58b2cdbe62cb0d811beb72b33273dee
mmwihaki <marymwihaki16@gmail.com>
2023-07-10 12:38:46 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,30 +6,30 @@
 #include <string.h>
 /**
 */
-int create_file(const char *filename, char text_content)
+int create_file(const char *filename, char *text_content)
 {
-int filedoc;
-ssize_t b_write;
-
-if (filename == NULL)
-return (-1);
-
-filedoc = open(filename, O_WRONLY | O_CREATE | O_TRUNC | S_IRUSR | S_IWUSR);
-
-if (filedoc = -1)
-return (-1);
-
-if (text_content != NULL)
-{
-b_write = write(filedoc, text_content, strlen(text_content));
-
-if (b_write == -1)
-{
-close(filedoc);
-return (-1);
-}
-}
-
-close(filedoc);
-return (-1);
+	int filedoc;
+	ssize_t b_write;
+	
+	if (filename == NULL)
+		return (-1);
+	
+	filedoc = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	
+	if (filedoc == -1)
+		return (-1);
+	
+	if (text_content != NULL)
+	{
+		b_write = write(filedoc, text_content, strlen(text_content));
+		
+		if (b_write == -1)
+		{
+			close(filedoc);
+			return (-1);
+		}
+	}
+	
+	close(filedoc);
+	return (1);
 }
